6cb92817dbb68bc2ccdc6713777b88a6a1a5cc85
Elena <aqvialet@gmail.com>
2021-11-01 03:31:10 +0300

    Update test

--- cypress/integration/tests/create_regular_user.js
+++ cypress/integration/tests/create_regular_user.js
@@ -1,7 +1,19 @@
+const { random } = require("lodash");
+
 describe('Create user with correct params', () => {
+
+    const userName='User_'+ random();
+
     it('Navigate to users name', () => {
         cy.visit('http://127.0.0.1:3000/');
         cy.get(':nth-child(2) > .nav-link').click();
         cy.get('.h2').should('have.text', 'Tasks');
-    })
-  })
+        cy.get('.btn').click();
+        cy.get('.h2').should('have.text', 'Create user');
+        cy.get('label').should('have.text', 'Name');
+        cy.get('.form-control').type(userName);
+        cy.get('.form-control').should('have.value', userName);
+        cy.get('.btn').click(); 
+        cy.get('.alert').should('have.text', 'User has been created')  
+    });
+  });
